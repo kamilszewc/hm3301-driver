6c2995cce207b9a3e2794b9fe3cd31c344594e3b
Dr. Kamil Szewc <kamil.szewc@essteyr.com>
2021-02-17 15:01:10 +0100

    Updated readme.md

--- hm3301.c
+++ hm3301.c
@@ -9,6 +9,7 @@ int measure_concentration(char* i2c_adapter, int* pm10, int *pm25, int *pm100) {
     int addr = 0x40;
     int reg = 0x88;
 
+
     int file = open(i2c_adapter, O_RDWR);
     if (file < 0) {
         printf("Device file can not be opened");
@@ -19,6 +20,8 @@ int measure_concentration(char* i2c_adapter, int* pm10, int *pm25, int *pm100) {
         printf("Error, can not specify device address I want to communicate");
         return -1;
     }
+
+    i2c_smbus_write_word_data(file, reg, reg);
 
     __u8 values[29];
     int res = i2c_smbus_read_i2c_block_data(file, reg, 29, values);
--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
 # HM3301 driver
 
-Very simple Raspberry Pi driver for hm3301 atmospheric particle concentration sensor.
+Very simple Raspberry Pi i2c driver for hm3301 atmospheric particle concentration sensor.
+Tested with Grove sensor.
